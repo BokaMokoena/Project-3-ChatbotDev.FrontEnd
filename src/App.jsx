0a77de1e2bbf7bee5fda57df23a98e3f96b83275
MaleselaModiba <222140275@mycput.ac.za>
2024-08-19 22:39:07 +0200

    Login and Chat interface

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,25 @@
-import React from 'react';
+// App.js
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import Chat from './Components/Chat';
+import Login from './Components/LoginPage';
 import './App.css';
-import Login from './components/LoginPage.jsx';
-
 
 function App() {
-  return (
-    <div className="App">
-      <Login />
-    </div>
-  );
+    return (
+        <Router>
+            <div style={{ padding: '20px' }}>
+                <nav>
+                    <Link to="/chat">Chat</Link> | <Link to="/login">Login</Link>
+                </nav>
+                <Routes>
+                    {/* Routes component should wrap around all Route components */}
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/chat" element={<Chat />} />
+                </Routes>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
+
